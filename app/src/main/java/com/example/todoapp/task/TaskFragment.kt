64aead94cd42e0e6a1d3883b4ede264914b8a2b7
FAVOUR <truelyfavoured@gmail.com>
2020-05-20 07:38:28 +0100

    Feature [ Adding of th drawerLayout ]- ongoing

---
+++ app/src/main/java/com/example/todoapp/task/TaskFragment.kt
@@ -0,0 +1,25 @@
+package com.example.todoapp.task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.todoapp.R
+
+class TaskFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return  inflater.inflate(R.layout.activity_task,container,false)
+    }
+
+
+
+
+}
