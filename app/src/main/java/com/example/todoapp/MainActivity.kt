9c01191a33ed4ad4bf3abc9cbba89ccafd32539e
FAVOUR <truelyfavoured@gmail.com>
2020-05-22 07:39:13 +0100

    Feature [ Integrating the fragment with Navigation Component]- ongoing

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -4,19 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var navController:NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navController = Navigation.findNavController(this,R.id.nav_host)
-        NavigationUI.setupWithNavController(nav_view,navController)
-        NavigationUI.setupActionBarWithNavController(this,navController,nav_drawer)
-
-
+        nav_view.setupWithNavController(navController)
+        appBarConfiguration =
+            AppBarConfiguration.Builder(R.id.tasks_fragment_dest, R.id.statistics_fragment_dest)
+                .setDrawerLayout(nav_drawer)
+                .build()
+//        NavigationUI.setupActionBarWithNavController(this,navController,nav_drawer)
+        setupActionBarWithNavController(navController,appBarConfiguration)
     }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
 }
