5fea44380f2b2e333d27e1eba2ab92e325477368
FAVOUR <truelyfavoured@gmail.com>
2020-05-26 11:38:40 +0100

    Feature [Setting up the Task fragment class]-Ongoing

--- app/src/main/java/com/example/todoapp/task/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/example/todoapp/task/viewmodel/TaskViewModel.kt
@@ -1,6 +1,67 @@
 package com.example.todoapp.task.viewmodel
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
 
 class TaskViewModel :ViewModel() {
+
+    private val tasksRepository = DefaultTasksRepository.getRepository(application)
+
+    private val _forceUpdate = MutableLiveData<Boolean>(false)
+
+    private val _items: LiveData<List<Task>> = _forceUpdate.switchMap { forceUpdate ->
+        if (forceUpdate) {
+            _dataLoading.value = true
+            viewModelScope.launch {
+                tasksRepository.refreshTasks()
+                _dataLoading.value = false
+            }
+        }
+        tasksRepository.observeTasks().switchMap { filterTasks(it) }
+
+    }
+
+    val items: LiveData<List<Task>> = _items
+
+    private val _dataLoading = MutableLiveData<Boolean>()
+    val dataLoading: LiveData<Boolean> = _dataLoading
+
+    private val _currentFilteringLabel = MutableLiveData<Int>()
+    val currentFilteringLabel: LiveData<Int> = _currentFilteringLabel
+
+    private val _noTasksLabel = MutableLiveData<Int>()
+    val noTasksLabel: LiveData<Int> = _noTasksLabel
+
+    private val _noTaskIconRes = MutableLiveData<Int>()
+    val noTaskIconRes: LiveData<Int> = _noTaskIconRes
+
+    private val _tasksAddViewVisible = MutableLiveData<Boolean>()
+    val tasksAddViewVisible: LiveData<Boolean> = _tasksAddViewVisible
+
+    private val _snackbarText = MutableLiveData<Event<Int>>()
+    val snackbarText: LiveData<Event<Int>> = _snackbarText
+
+    private var currentFiltering = TasksFilterType.ALL_TASKS
+
+    // Not used at the moment
+    private val isDataLoadingError = MutableLiveData<Boolean>()
+
+    private val _openTaskEvent = MutableLiveData<Event<String>>()
+    val openTaskEvent: LiveData<Event<String>> = _openTaskEvent
+
+    private val _newTaskEvent = MutableLiveData<Event<Unit>>()
+    val newTaskEvent: LiveData<Event<Unit>> = _newTaskEvent
+
+    private var resultMessageShown: Boolean = false
+
+    // This LiveData depends on another so we can use a transformation.
+    val empty: LiveData<Boolean> = Transformations.map(_items) {
+        it.isEmpty()
+    }
+
+    init {
+        // Set initial state
+        setFiltering(TasksFilterType.ALL_TASKS)
+        loadTasks(true)
+    }
 }
