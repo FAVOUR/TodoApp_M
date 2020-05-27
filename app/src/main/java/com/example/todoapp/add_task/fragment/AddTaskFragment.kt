67a5244abdbf94473abf117a8b24bd6133c8d14c
FAVOUR <truelyfavoured@gmail.com>
2020-05-27 22:33:50 +0100

    Feature [Setting up the AddTask Fragment ]- A few cleanups while Fixing bugs

--- app/src/main/java/com/example/todoapp/add_task/fragment/AddTaskFragment.kt
+++ app/src/main/java/com/example/todoapp/add_task/fragment/AddTaskFragment.kt
@@ -1,6 +1,5 @@
 package com.example.todoapp.add_task.fragment
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
