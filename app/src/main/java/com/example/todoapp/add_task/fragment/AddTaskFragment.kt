67d726719dbac4e5f734efd951c96e06ff04ae45
FAVOUR <truelyfavoured@gmail.com>
2020-05-27 22:32:22 +0100

    Feature [Setting up the AddTask Fragment ]- Done

--- app/src/main/java/com/example/todoapp/add_task/fragment/AddTaskFragment.kt
+++ app/src/main/java/com/example/todoapp/add_task/fragment/AddTaskFragment.kt
@@ -14,49 +14,50 @@ import com.example.todoapp.EventObserver
 
 import com.example.todoapp.R
 import com.example.todoapp.add_task.viewmodel.AddTaskViewModel
+import com.example.todoapp.databinding.AddtaskFragmentBinding
 import com.example.todoapp.util.setupRefreshLayout
 import com.example.todoapp.util.setupSnackbar
 import com.google.android.material.snackbar.Snackbar
 
 class AddTaskFragment : Fragment() {
 
-//    private lateinit var viewDataBinding: AddtaskFragBinding
-//
-//    private val args: AddEditTaskFragmentArgs by navArgs()
-//
-//    private val viewModel by viewModels<AddEditTaskViewModel>()
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        val root = inflater.inflate(R.layout.addtask_frag, container, false)
-//        viewDataBinding = AddtaskFragBinding.bind(root).apply {
-//            this.viewmodel = viewModel
-//        }
-//        // Set the lifecycle owner to the lifecycle of the view
-//        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
-//        return viewDataBinding.root
-//    }
-//
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//        setupSnackbar()
-//        setupNavigation()
-//        this.setupRefreshLayout(viewDataBinding.refreshLayout)
-//        viewModel.start(args.taskId)
-//    }
-//
-//    private fun setupSnackbar() {
-//        view?.setupSnackbar(this, viewModel.snackbarText, Snackbar.LENGTH_SHORT)
-//    }
-//
-//    private fun setupNavigation() {
-//        viewModel.taskUpdatedEvent.observe(this, EventObserver {
-//            val action = AddEditTaskFragmentDirections
-//                .actionAddEditTaskFragmentToTasksFragment(ADD_EDIT_RESULT_OK)
-//            findNavController().navigate(action)
-//        })
-//    }
+    private lateinit var viewDataBinding: AddtaskFragmentBinding
+
+    private val args: AddTaskFragmentArgs by navArgs()
+
+    private val viewModel by viewModels<AddTaskViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.addtask_fragment, container, false)
+        viewDataBinding = AddtaskFragmentBinding.bind(root).apply {
+            this.viewmodel = viewModel
+        }
+        // Set the lifecycle owner to the lifecycle of the view
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        return viewDataBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupSnackbar()
+        setupNavigation()
+        this.setupRefreshLayout(viewDataBinding.refreshLayout)
+        viewModel.start(args.taskId)
+    }
+
+    private fun setupSnackbar() {
+        view?.setupSnackbar(this, viewModel.snackbarText, Snackbar.LENGTH_SHORT)
+    }
+
+    private fun setupNavigation() {
+        viewModel.taskUpdatedEvent.observe(this, EventObserver {
+            val action = AddTaskFragmentDirections
+                .actionAddEditTaskFragmentToTasksFragment(ADD_EDIT_RESULT_OK)
+            findNavController().navigate(action)
+        })
+    }
 
 }
