f83f7730b078acf542f513313122f00babab91b3
FAVOUR <truelyfavoured@gmail.com>
2020-05-29 07:53:04 +0100

    Feature [Write Unit Test for TaskViewModel] -Writing test for the addNewTask method

---
+++ app/src/test/java/com/example/todoapp/task/viewmodel/TaskViewModelTest.kt
@@ -0,0 +1,13 @@
+package com.example.todoapp.task.viewmodel
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class TaskViewModelTest{
+
+    @Test
+    fun addTask_setsNewEventTask(){}
+}
