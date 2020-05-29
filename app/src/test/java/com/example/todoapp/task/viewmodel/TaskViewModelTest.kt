fcebb61e7bd369164e6d722931aa501f195613bd
FAVOUR <truelyfavoured@gmail.com>
2020-05-29 13:44:48 +0100

    Feature [Write Unit Test for TaskViewModel] - Partly wrote  test for the addNewTask method

--- app/src/test/java/com/example/todoapp/task/viewmodel/TaskViewModelTest.kt
+++ app/src/test/java/com/example/todoapp/task/viewmodel/TaskViewModelTest.kt
@@ -1,13 +1,27 @@
 package com.example.todoapp.task.viewmodel
 
+import android.app.Application
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
-@RunWith(JUnit4::class)
+@RunWith(AndroidJUnit4::class)
 class TaskViewModelTest{
 
     @Test
-    fun addTask_setsNewEventTask(){}
+    fun addTask_setsNewEventTask(){
+
+        //Given
+        var app = ApplicationProvider.getApplicationContext<Application>()
+
+        var taskVM= TaskViewModel(app)
+
+        //When
+         taskVM.addNewTask()
+
+        //
+    }
 }
