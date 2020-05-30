a62d788b839084204498cf142b310e029a017290
FAVOUR <truelyfavoured@gmail.com>
2020-05-30 19:22:38 +0100

    Feature [Write Unit Test for TaskViewModel] - Done

---
+++ app/src/test/java/com/example/todoapp/LiveDataHelper.kt
@@ -0,0 +1,24 @@
+package com.example.todoapp
+
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+
+
+fun <T> addObserver(){
+
+
+     val observer = object :Observer<T>{
+         override fun onChanged(t: T) {
+
+         }
+     }
+
+    try {
+
+    }
+    catch (e:Exception){
+
+    }finally {
+
+    }
+ }
--- app/src/test/java/com/example/todoapp/task/viewmodel/TaskViewModelTest.kt
+++ app/src/test/java/com/example/todoapp/task/viewmodel/TaskViewModelTest.kt
@@ -1,9 +1,17 @@
 package com.example.todoapp.task.viewmodel
 
 import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.databinding.Observable
+import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.todoapp.Event
+import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.nullValue
+import org.jetbrains.annotations.NotNull
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -11,6 +19,8 @@ import org.junit.runners.JUnit4
 @RunWith(AndroidJUnit4::class)
 class TaskViewModelTest{
 
+    @get:Rule
+    var instantTaskExecutorRule =InstantTaskExecutorRule()
     @Test
     fun addTask_setsNewEventTask(){
 
@@ -19,9 +29,25 @@ class TaskViewModelTest{
 
         var taskVM= TaskViewModel(app)
 
-        //When
-         taskVM.addNewTask()
 
         //
+
+        var observer = Observer<Event<Unit>>{}
+
+        try {
+            taskVM.newTaskEvent.observeForever(observer)
+
+
+            //When
+            taskVM.addNewTask()
+
+
+            var value = taskVM.newTaskEvent.value
+
+            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+
+        }finally {
+            taskVM.newTaskEvent.removeObserver(observer)
+        }
     }
 }
