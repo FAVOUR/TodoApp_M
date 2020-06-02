89a9209fbdf435b541485b2cca79817742703b66
FAVOUR <truelyfavoured@gmail.com>
2020-06-02 08:26:14 +0100

    Feature [Write Unit Test] - Create false TaskRepository class  with to mimic DefaultTaskRepository class

---
+++ app/src/test/java/com/example/todoapp/data/source/FakeTaskRepositoryTest.kt
@@ -0,0 +1,81 @@
+package com.example.todoapp.data.source
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.todoapp.data.Task
+import com.example.todoapp.util.Result
+import kotlinx.coroutines.runBlocking
+
+class FakeTaskRepositoryTest :TaskRepository{
+
+    var taskDataSource :LinkedHashMap<String,Task> = LinkedHashMap()
+    var  observableDataSource : MutableLiveData<Result<List<Task>>> = MutableLiveData()
+
+    override fun observeTasks(): LiveData<Result<List<Task>>> {
+        runBlocking { refreshTasks() }
+        return  observableDataSource
+    }
+
+    override suspend fun getTask(isForceUpdate: Boolean): Result<List<Task>> {
+             val tasks=  taskDataSource.values.toList()
+//            Log.i("tasks", Gson())
+           return  Result.Success(tasks)
+    }
+
+    override suspend fun getTask(taskId: String, forceUpdate: Boolean): Result<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun refreshTasks() {
+      observableDataSource.value = getTask()
+    }
+
+    override suspend fun refreshTasks(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeTask(taskId: String): LiveData<Result<Task>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun completeTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun completeTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun clearCompletedTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAllTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    private fun addTask(vararg tasks:Task){
+            for(task in tasks ) {
+                taskDataSource[task.id] = task
+            }
+        }
+
+
+}
