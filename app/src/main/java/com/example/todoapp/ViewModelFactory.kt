453daeade889d3e22382d64d8d8ff8e971e86182
FAVOUR <truelyfavoured@gmail.com>
2020-06-04 18:09:44 +0100

    Feature [Write Unit Test] - Refactor the ViewModelFactory classes to build the viewmodel Tested

--- app/src/main/java/com/example/todoapp/ViewModelFactory.kt
+++ app/src/main/java/com/example/todoapp/ViewModelFactory.kt
@@ -11,9 +11,10 @@ import com.example.todoapp.task_detail.viewmodel.TaskDetailViewModel
 @Suppress("UNCHECKED_CAST")
 class ViewModelFactory (val  tasksRepository : TaskRepository):ViewModelProvider.Factory{
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            with(modelClass){
-                when{
+    override fun <T : ViewModel?> create(modelClass: Class<T>) :T =
+
+        with(modelClass){
+                 when{
                     isAssignableFrom(AddTaskViewModel::class.java) -> AddTaskViewModel(tasksRepository)
                     isAssignableFrom(StatisticsViewModel::class.java) -> StatisticsViewModel(tasksRepository)
                     isAssignableFrom(TaskDetailViewModel::class.java) -> TaskDetailViewModel(tasksRepository)
@@ -23,32 +24,8 @@ class ViewModelFactory (val  tasksRepository : TaskRepository):ViewModelProvider
 
                 }
 
-            }
-          if (modelClass.isAssignableFrom(TaskViewModel::class.java)){
-               return  TaskViewModel(tasksRepository) as T
-          }else{
-              throw Throwable("the class $modelClass is not an instance of viewmodel ")
-          }
-    }
+            } as T
 
-
-
-//
-//    override fun <T : ViewModel> create(modelClass: Class<T>) =
-//        with(modelClass) {
-//            when {
-//                isAssignableFrom(StatisticsViewModel::class.java) ->
-//                    StatisticsViewModel(tasksRepository)
-//                isAssignableFrom(TaskDetailViewModel::class.java) ->
-//                    TaskDetailViewModel(tasksRepository)
-//                isAssignableFrom(AddEditTaskViewModel::class.java) ->
-//                    AddEditTaskViewModel(tasksRepository)
-//                isAssignableFrom(TasksViewModel::class.java) ->
-//                    TasksViewModel(tasksRepository)
-//                else ->
-//                    throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
-//            }
-//        } as T
 
 
 }
