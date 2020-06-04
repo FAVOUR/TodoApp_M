20b51724dc2fe346b7f52a254f2134ebe9562612
FAVOUR <truelyfavoured@gmail.com>
2020-06-04 15:59:32 +0100

    Feature [Write Unit Test] - Setting up the ServiceLocator.kt class adding the createDataSource Repository

--- app/src/main/java/com/example/todoapp/ServiceLocator.kt
+++ app/src/main/java/com/example/todoapp/ServiceLocator.kt
@@ -2,12 +2,17 @@ package com.example.todoapp
 
 import android.content.Context
 import androidx.room.Room
+import com.example.todoapp.data.source.DefaultTaskRepository
 import com.example.todoapp.data.source.TaskRepository
 import com.example.todoapp.data.source.TasksDataSource
+import com.example.todoapp.data.source.local.TaskLocalDataSource
 import com.example.todoapp.data.source.local.db.TaskDao
 import com.example.todoapp.data.source.local.db.ToDoDataBase
+import com.example.todoapp.data.source.remote.TaskRemoteDataSource
 
 object ServiceLocator {
+
+    var dataBase :ToDoDataBase? =null
 
     //Create a TaskRepository and if created return the already instantiated instance
 //     fun provideRepository():TasksDataSource{
@@ -15,21 +20,25 @@ object ServiceLocator {
 //    }
 //
 //    //Create the TaskDataRepository
-//     fun taskDataRepository():TaskRepository{
-//
+//     fun taskDataRepository(context: Context):TaskRepository{
+//       val  defaultRepository = DefaultTaskRepository(TaskRemoteDataSource,createLocalDataSource(context))
 //    }
-//
-//    //Create a TaskDataSource
-//    fun createLocalDataSource():TaskDataSource{
-//
-//    }
+
+    //Create a TaskDataSource
+    fun createLocalDataSource(context: Context):TasksDataSource{
+        val dataBase = dataBase ?: createDatabase(context)
+
+        return  TaskLocalDataSource(dataBase.taskDoa())
+    }
 
     //Create the repository
     fun createDatabase(context: Context):ToDoDataBase{
 
-        var dbCreated = Room.databaseBuilder(context,ToDoDataBase::class.java,"todo_db")
+        var dbCreated = Room.databaseBuilder(context,ToDoDataBase::class.java,"Tasks.db")
                                         .fallbackToDestructiveMigration()
                                         .build()
+        dataBase=dbCreated
+
         return dbCreated
     }
 
