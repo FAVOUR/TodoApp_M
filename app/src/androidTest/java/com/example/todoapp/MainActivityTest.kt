e0dbc6ed2d03342b80a430d1d1df6f315bcb8279
FAVOUR <truelyfavoured@gmail.com>
2020-06-12 17:24:00 +0100

    Feature [Write Unit Test] -Wrote expresso test for the MainActivity.kt

--- app/src/androidTest/java/com/example/todoapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/todoapp/MainActivityTest.kt
@@ -1,176 +1,91 @@
 package com.example.todoapp
 
 
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+import android.graphics.ColorSpace.match
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.`is`
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
+import com.example.todoapp.data.Task
+import com.example.todoapp.data.source.TaskRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
+@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
-class MainActivityTest {
+class MainActivityTest{
 
-    @Rule
-    @JvmField
-    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+    lateinit var repository: TaskRepository
 
-    @Test
-    fun mainActivityTest() {
-        val appCompatCheckBox = onView(
-            allOf(
-                withId(R.id.complete_checkbox),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.tasks_list),
-                        0
-                    ),
-                    0
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatCheckBox.perform(click())
+//    @ExperimentalCoroutinesApi
+//    @get:Rule
+//    var testCouroutine= TestCouroutinUtilAT()
 
-        val recyclerView = onView(
-            allOf(
-                withId(R.id.tasks_list),
-                childAtPosition(
-                    withId(R.id.tasks_linear_layout),
-                    1
-                )
-            )
-        )
-        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+    @Before
+    fun  setUp(){
+//        val context=ApplicationProvider.getApplicationContext<TodoApplication>()
+         repository=  ServiceLocator.provideRepository(getApplicationContext())
 
-        val appCompatImageButton = onView(
-            allOf(
-                withContentDescription("Navigate up"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.action_bar),
-                        childAtPosition(
-                            withId(R.id.action_bar_container),
-                            0
-                        )
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatImageButton.perform(click())
-
-        val recyclerView2 = onView(
-            allOf(
-                withId(R.id.tasks_list),
-                childAtPosition(
-                    withId(R.id.tasks_linear_layout),
-                    1
-                )
-            )
-        )
-        recyclerView2.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
-
-        val floatingActionButton = onView(
-            allOf(
-                withId(R.id.edit_task_fab),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.coordinator_layout),
-                        childAtPosition(
-                            withId(R.id.nav_host),
-                            0
-                        )
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        floatingActionButton.perform(click())
-
-        val appCompatEditText = onView(
-            allOf(
-                withId(R.id.add_task_description_edit_text),
-                withText("Ground looks good, no foundation work required."),
-                childAtPosition(
-                    childAtPosition(
-                        withClassName(`is`("android.widget.ScrollView")),
-                        0
-                    ),
-                    1
-                )
-            )
-        )
-        appCompatEditText.perform(
-            scrollTo(),
-            replaceText("Ground looks good, no foundation work required. viola")
-        )
-
-        val appCompatEditText2 = onView(
-            allOf(
-                withId(R.id.add_task_description_edit_text),
-                withText("Ground looks good, no foundation work required. viola"),
-                childAtPosition(
-                    childAtPosition(
-                        withClassName(`is`("android.widget.ScrollView")),
-                        0
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatEditText2.perform(closeSoftKeyboard())
-
-        val floatingActionButton2 = onView(
-            allOf(
-                withId(R.id.save_task_fab),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.coordinator_layout),
-                        childAtPosition(
-                            withId(R.id.nav_host),
-                            0
-                        )
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        floatingActionButton2.perform(click())
-    }
-
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
-    ): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
+        runBlocking{
+            repository.deleteAllTasks()
         }
     }
+
+    @After
+    fun reset(){
+        ServiceLocator.resetRepository()
+    }
+
+
+    @Test
+    fun editText()= runBlocking {
+        var task = Task("First Expresso test","Initial Setup")
+        repository.saveTask(task)
+
+        var activitySenerio = ActivityScenario.launch(MainActivity::class.java)
+
+
+         onView(withText("First Expresso test")).perform(click())
+         onView(withId(R.id.task_detail_title_text)).check( matches(withText("First Expresso test")))
+         onView(withId(R.id.task_detail_description_text)).check( matches(withText("Initial Setup")))
+         onView(withId(R.id.task_detail_complete_checkbox)).check( matches(isNotChecked()))
+
+
+        //Click edit save
+        onView(withId(R.id.edit_task_fab)).perform(click())
+        onView(withId(R.id.add_task_title_edit_text)).perform(replaceText("NEW TITLE"))
+        onView(withId(R.id.add_task_description_edit_text)).perform(replaceText("NEW DESCRIPTION"))
+        onView(withId(R.id.save_task_fab)).perform(click())
+
+
+        // Verify task is displayed on screen in the task list.
+        onView(withText("NEW TITLE")).check(matches(isDisplayed()))
+        // Verify previous task is not displayed
+        onView(withText("TITLE1")).check(ViewAssertions.doesNotExist())
+
+
+        delay(5000)
+
+
+        activitySenerio.close()
+    }
+
+
+
 }
