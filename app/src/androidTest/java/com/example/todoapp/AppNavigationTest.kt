82a0f9fb7c08cc55dd58fb6c65132d0d092a9f9f
FAVOUR <truelyfavoured@gmail.com>
2020-06-14 12:49:12 +0100

    Feature [Write Unit Test] -Initial St up for writing test for AppNavigationTest.kt class .

--- app/src/androidTest/java/com/example/todoapp/AppNavigationTest.kt
+++ app/src/androidTest/java/com/example/todoapp/AppNavigationTest.kt
@@ -1,6 +1,35 @@
 package com.example.todoapp
 
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.todoapp.util.DataBindingIdlingResource
+import com.example.todoapp.util.ExpressoIdling
+import org.junit.After
+import org.junit.Before
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
 class AppNavigationTest {
+
+    val espressoIdlingResource =DataBindingIdlingResource()
+
+    @Before
+    fun initialSetup(){
+
+        IdlingRegistry.getInstance().register(ExpressoIdling.countdownIdlingResource)
+        IdlingRegistry.getInstance().register(espressoIdlingResource)
+
+    }
+
+    @After
+    fun reset(){
+        IdlingRegistry.getInstance().unregister(ExpressoIdling.countdownIdlingResource)
+        IdlingRegistry.getInstance().unregister(espressoIdlingResource)
+
+    }
+
 
 
 }
