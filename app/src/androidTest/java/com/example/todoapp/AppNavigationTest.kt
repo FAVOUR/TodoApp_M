539be7e59e9cbe2cf3db74171437f0059e46cf77
FAVOUR <truelyfavoured@gmail.com>
2020-06-14 17:09:05 +0100

    Feature [Write Unit Test] -Done with writing test for testing the back arrow  AppNavigationTest.kt class .

--- app/src/androidTest/java/com/example/todoapp/AppNavigationTest.kt
+++ app/src/androidTest/java/com/example/todoapp/AppNavigationTest.kt
@@ -4,20 +4,26 @@ import android.app.Activity
 import android.graphics.ColorSpace.match
 import android.view.Gravity
 import androidx.appcompat.widget.Toolbar
+import androidx.room.Room
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerMatchers.isClosed
 import androidx.test.espresso.contrib.DrawerMatchers.isOpen
-import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.example.todoapp.data.Task
+import com.example.todoapp.data.source.TaskRepository
+import com.example.todoapp.data.source.local.db.TaskDao
+import com.example.todoapp.data.source.local.db.ToDoDataBase
 import com.example.todoapp.util.DataBindingIdlingResource
 import com.example.todoapp.util.ExpressoIdling
 import com.example.todoapp.util.monitorActivity
+import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -28,6 +34,29 @@ import org.junit.runner.RunWith
 class AppNavigationTest {
 
     val dataBindingIdlingResource =DataBindingIdlingResource()
+     lateinit var  taskRepository : TaskRepository
+     lateinit var  taskDataBase : ToDoDataBase
+
+    @Before
+    fun init(){
+        taskDataBase= Room.inMemoryDatabaseBuilder(getApplicationContext(),ToDoDataBase::class.java)
+                            .allowMainThreadQueries()
+                            .build()
+
+        taskRepository =  ServiceLocator.provideRepository(getApplicationContext())
+
+        runBlocking {
+            taskRepository.deleteAllTasks()
+        }
+
+    }
+
+    @After
+    fun resetDetails(){
+        taskDataBase.close()
+    }
+
+
 
     @Before
     fun initialSetup(){
@@ -61,6 +90,35 @@ class AppNavigationTest {
         // When using ActivityScenario.launch(), always call close()
         activityScenario.close()
     }
+
+
+    @Test
+    fun taskDetailScreen_doubleUpButton() = runBlocking {
+        val task = Task("Up button", "Description")
+        taskRepository.saveTask(task)
+
+        // Start the Tasks screen.
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // 1. Click on the task on the list.
+        onView(withText("Up button")).perform(click())
+
+        // 2. Click on the edit task button.
+        onView(withId(R.id.edit_task_fab)).perform(click())
+        // 3. Confirm that if we click Up button once, we end up back at the task details page.
+        onView(withContentDescription(activityScenario.getToolbarNavigationContentDescription())).perform(click())
+        // 4. Confirm that if we click Up button a second time, we end up back at the home screen.
+
+        onView(withContentDescription(activityScenario.getToolbarNavigationContentDescription())).perform(click())
+
+        onView(withId(R.id.tasks_container_layout)).check(matches(isDisplayed()))
+        // When using ActivityScenario.launch(), always call close().
+        activityScenario.close()
+    }
+
+
+
 
 
 
